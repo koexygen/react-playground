5c1df9c30b6aa06f5ba0aea3230dc86f5215bc0b
Koexygen <rexarkoexygen@gmail.com>
2021-01-03 11:18:33 +0400

    playing with hooks

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import Accordion from './components/Accordion'
+
+const items = [
+    {title: 'What is React?', content: 'React is javascript front-end library'},
+    {title: 'What is React2?', content: 'React is javascript front-end library'},
+    {title: 'What is React3?', content: 'React is javascript front-end library'},
+]
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>hi</h1>
+        <Accordion items={items}/>
     </div>
   );
 }
---
+++ src/components/Accordion.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Accordion = ({items}) => {
+    const renderedItems = items.map((item) => {
+
+    })
+
+    return <h1>accordion</h1>
+}
+
+export default Accordion
