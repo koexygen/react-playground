773a64f008682a356a674c0a082c289dec371cf5
Koexygen <rexarkoexygen@gmail.com>
2021-01-03 11:33:38 +0400

    playing with hooks

--- src/App.js
+++ src/App.js
@@ -1,16 +1,21 @@
-import Accordion from './components/Accordion'
+import Accordion from "./components/Accordion";
 
 const items = [
-    {title: 'What is React?', content: 'React is javascript front-end library'},
-    {title: 'What is React2?', content: 'React is javascript front-end library'},
-    {title: 'What is React3?', content: 'React is javascript front-end library'},
-]
+  { title: "What is React?", content: "React is javascript front-end library" },
+  {
+    title: "What is React2?",
+    content: "React is javascript front-end library",
+  },
+  {
+    title: "What is React3?",
+    content: "React is javascript front-end library",
+  },
+];
 
 function App() {
   return (
     <div className="App">
-      <h1>hi</h1>
-        <Accordion items={items}/>
+      <Accordion items={items} />
     </div>
   );
 }
--- src/components/Accordion.js
+++ src/components/Accordion.js
@@ -1,11 +1,21 @@
-import React from 'react';
+import React from "react";
 
-const Accordion = ({items}) => {
-    const renderedItems = items.map((item) => {
+const Accordion = ({ items }) => {
+  const renderedItems = items.map((item) => {
+    return (
+      <React.Fragment key={item.title}>
+        <div className="title">
+          <i className="dropdown icon" />
+          {item.title}
+        </div>
+        <div className="content">
+          <p className="transition hidden">{item.content}</p>
+        </div>
+      </React.Fragment>
+    );
+  });
 
-    })
+  return <div className="ui styled accordion">{renderedItems}</div>;
+};
 
-    return <h1>accordion</h1>
-}
-
-export default Accordion
+export default Accordion;
