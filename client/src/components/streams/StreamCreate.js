ba88c87f4a99b4b9e80dc784fa9a3a0a73d5fe28
Koexygen <rexarkoexygen@gmail.com>
2021-01-18 19:31:55 +0400

    bump

--- client/src/components/streams/StreamCreate.js
+++ client/src/components/streams/StreamCreate.js
@@ -2,15 +2,35 @@ import React from "react";
 import { Field, reduxForm } from "redux-form";
 
 class StreamCreate extends React.Component {
-  renderInput({ input }) {
-    return <input {...input} />;
+  renderInput({ input, label }) {
+    return (
+      <div className="item">
+        <label>{label}</label>
+        <input {...input} />
+      </div>
+    );
+  }
+
+
+
+
+  onSubmit(values) {
+    console.log(values);
   }
 
   render() {
     return (
-      <form>
-        <Field name="title" component={this.renderInput} />
-        <Field name="description" component={this.renderInput} />
+      <form
+        className="ui form"
+        onSubmit={this.props.handleSubmit(this.onSubmit)}
+      >
+        <Field name="title" component={this.renderInput} label="Enter title" />
+        <Field
+          name="description"
+          component={this.renderInput}
+          label="Enter description"
+        />
+        <button className="ui button primary">Submit</button>
       </form>
     );
   }
